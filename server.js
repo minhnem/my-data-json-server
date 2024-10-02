ef4dd53e2d513cc08aae855e7447e5589a904061
minhnem <dtc2154801030092@ictu.edu.vn>
2024-10-02 08:32:50 +0700

    update01

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');  // Kết nối với file db.json
-const middlewares = jsonServer.defaults();    // Các middleware mặc định
+const middlewares = jsonServer.defaults();
+
+// Dữ liệu mô phỏng thay vì đọc từ file db.json
+const data = {
+  "posts": [
+    { "id": 1, "title": "Hello World" }
+  ]
+};
+
+// Sử dụng bộ nhớ thay vì file
+const router = jsonServer.router(data);
 
 server.use(middlewares);
 server.use(router);
-server.listen(process.env.PORT || 3000, () => {  // Sử dụng process.env.PORT cho Vercel
+
+// Sử dụng process.env.PORT cho Vercel
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
